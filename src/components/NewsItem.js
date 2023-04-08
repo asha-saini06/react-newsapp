f3573d56418e6e95faa31fb61b7cd3df57144e85
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2023-04-08 18:28:28 +0530

    Component Structure of our NewsMonkey React App

---
+++ src/components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    return <div>This is a New Item ...</div>;
+  }
+}
+
+export default NewsItem;
